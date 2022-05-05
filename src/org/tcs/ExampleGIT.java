4b58fba0f794dcfbbf1c7d720da7ea7569d9d26e
hariharan3793 <hariharansingaram3793@gmail.com>
2022-05-05 14:16:31 +0530

    added

---
+++ src/org/tcs/ExampleGIT.java
@@ -0,0 +1,15 @@
+package org.tcs;
+
+public class ExampleGIT {
+	
+	private void KRIYANSH() {
+		System.out.println("KRISH");
+
+	}
+	
+	public static void main(String[] args) {
+		ExampleGIT ex=new ExampleGIT();
+		ex.KRIYANSH();
+	}
+
+}
